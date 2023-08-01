46ace1612a42bff70a498d33e0b8b0dd80bf2142
Xiang Zheng <68246040+archmyth@users.noreply.github.com>
2023-08-01 22:39:20 +0000

    1st

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Characters,Planets,Favorites
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,109 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+# START endpoints
+@app.route('/user/<int:user_character_id>', methods=['GET'])
+def find_user(user_character_id):
+    user_character_id = User.query.get(user_character_id)
+    if not user_character_id:
+        return jsonify(message='user not found'), 404
+    return jsonify(user_character_id)
+
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def Get_users():
+    users = User.query.all()
+    all_users = list(map(lambda x: x.serialize(), users))
+    return jsonify(all_users), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
 
-    return jsonify(response_body), 200
+# create one user
+@app.route('/user', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    email = data.get('email')
+    new_user = User(email=email, password=password, username=username)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify('new user created'), 200
+
+@app.route('/user/favorites', methods=['GET'])
+def user_faves():
+    favorites = Favorites.query.all()
+    all_favorites = list(map(lambda x: x.serialize(), favorites))
+    return jsonify(all_favorites), 200
+
+@app.route('/user/<int:user_character_id>', methods=['DELETE'])
+def delete_user(user_character_id):
+    user = User.query.get(user_character_id)
+    if user is None:
+        return jsonify('User not found'), 404
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify('User deleted'), 200
+
+
+
+#CHARACTERS
+#get_all_charaters
+@app.route('/characters', methods=['GET'])
+def get_all_characters():
+    characters = Characters.query.all()
+    results = list(map(lambda item: item.serialize(), characters))
+    return jsonify(results), 200
+
+@app.route('/characters/<int:character_id>', methods=['GET'])
+def get_one_character(character_id):
+    character = Characters.query.get(character_id)
+    return jsonify(character.serialize()), 200
+
+@app.route('/favorites/characters/<int:character_id>', methods=['POST'])
+def fave_characters(character_id):
+    favorite_characters = Favorites(user_character_id=request.get_json()['user_character_id'], characters_id=character_id)
+    db.session.add(favorite_characters)
+    db.session.commit()
+    return jsonify('Favorite character added'), 200
+#PLANETS
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    planets_query = Planets.query.all()
+    results = list(map(lambda item: item.serialize(), planets_query))
+    return jsonify(results), 200
+
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_one_planets(planet_id):
+    planet_query = Planets.query.get(planet_id)
+    return jsonify(planet_query.serialize()), 200
+
+@app.route('/favorites/planets/<int:planet_id>', methods=['POST'])
+def fave_planets(planet_id):
+    favorite_planets = Favorites(user_character_id=request.get_json()['user_character_id'], planets_id=planet_id)
+    db.session.add(favorite_planets)
+    db.session.commit()
+    return jsonify('Favorite planet added'), 200
+
+#Favorites
+@app.route('/favorites/planets/<int:planets_id>', methods=['DELETE'])
+def delete_favorite_planet(planets_id):
+    favorite_planets = Favorites.query.filter_by(user_character_id = request.get_json()['user_character_id'], planet_id=planets_id).first()
+    if favorite_planets is None:
+        return jsonify('Planet not found')
+    db.session.delete(favorite_planets)
+    return jsonify('favorite planet deleted')
+
+
+@app.route('/favorites/characters/<int:character_id>', methods=['DELETE'])
+def delete_favorite_characters(character_id):
+    favorite_characters = Favorites.query.filter_by(user_character_id = request.get_json()['user_character_id'], character_id=character_id).first()
+    if favorite_characters is None:
+        return jsonify('characters not found')
+    db.session.delete(favorite_characters)
+    return jsonify('favorite characters deleted')
+# END endpoints
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,92 @@
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    __tablename__ = 'user'
+    user_character_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    favorites = db.relationship('Favorites')
 
     def __repr__(self):
         return '<User %r>' % self.username
 
     def serialize(self):
         return {
-            "id": self.id,
+            "user_character_id": self.user_character_id,
+            "username": self.username,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Characters(db.Model):
+    __tablename__ = 'characters'
+    character_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    birth_year = db.Column(db.Integer)
+    gender = db.Column(db.String(250))
+    height = db.Column(db.Integer)
+    skin_color = db.Column(db.String(250))
+    eye_color = db.Column(db.String(250))
+    favorites = db.relationship('Favorites', backref='character')
+
+    def __repr__(self):
+        return '<Characters %r>' % self.character_id
+
+    def serialize(self):
+        return {
+            "character_id": self.character_id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "height": self.height,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+        }
+
+class Planets(db.Model):
+    __tablename__ = 'planets'
+    planet_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(250), unique=True)
+    climate = db.Column(db.String(20))
+    population = db.Column(db.Integer)
+    orbital_period = db.Column(db.Integer)
+    rotation_period = db.Column(db.Integer)
+    diameter = db.Column(db.Integer)
+    favorites = db.relationship('Favorites', backref='planet')
+
+    def __repr__(self):
+        return '<Planets %r>' % self.planet_id
+
+    def serialize(self):
+        return {
+            "planet_id": self.planet_id,
+            "name": self.name,
+            "climate": self.climate,
+            "population": self.population,
+            "orbital_period": self.orbital_period,
+            "rotation_period": self.rotation_period,
+            "diameter": self.diameter,
+        }
+
+class Favorites(db.Model):
+    __tablename__ = 'favorites'
+    favorite_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_character_id = db.Column(db.Integer, db.ForeignKey('user.user_character_id'))
+    characters_id = db.Column(db.Integer, db.ForeignKey('characters.character_id'))
+    planets_id = db.Column(db.Integer, db.ForeignKey('planets.planet_id'))
+
+    def __repr__(self):
+        return '<Favorites %r>' % self.favorite_id
+
+    def serialize(self):
+        return {
+            "favorite_id": self.favorite_id,
+            "characters_id": self.characters_id,
+            "planets_id": self.planets_id,
+            "user_character_id": self.user_character_id
         }
